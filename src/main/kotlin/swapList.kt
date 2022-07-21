9124cf7c13c2f8fa9884981ee1385db216f187d7
Saurabh <sunnykolhe2504@gmail.com>
2022-07-21 09:31:05 +0530

    print

--- src/main/kotlin/swapList.kt
+++ src/main/kotlin/swapList.kt
@@ -3,5 +3,5 @@ import java.util.*
 fun main() {
     val nums = arrayOf(10, 20, 30, 40, 50)
     Collections.swap(nums.asList(), 1, 3)
-    println(nums.contentToString())
+    println(nums.contentToString())// print
 }
